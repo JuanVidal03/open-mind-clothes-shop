3bf63c64bab0ce5129d96c45970c27fc9a8055d4
Juan Vidal <juanmvg2003@gmail.com>
2022-09-30 12:36:42 -0500

    fueron agregados los productos en archivo json e imagenes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -23,10 +23,10 @@ const Navbar = () =>{
 
             <nav className="navbar__container">
                 <ul className= {`navbar ${clicked ? 'active' : ''}`}>
+                    <li className="navbar__item"><a href="#" onClick={handleClick}>Inicio</a></li>
                     <li className="navbar__item"><a href="#" onClick={handleClick}>Hombre</a></li>
                     <li className="navbar__item"><a href="#" onClick={handleClick}>Mujer</a></li>
                     <li className="navbar__item"><a href="#" onClick={handleClick}>Niños</a></li>
-                    <li className="navbar__item"><a href="#" onClick={handleClick}>Nuevo</a></li>
 
                 </ul>
             </nav>
