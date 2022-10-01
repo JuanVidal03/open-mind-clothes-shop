97dd0946d3e3f1ee2fbd1b272a64b24b39261994
Juan Vidal <juanmvg2003@gmail.com>
2022-10-01 14:55:21 -0500

    ItemDetaif fue creado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+
 
 function App() {
+
+  const test = {
+    "Imagen": "/productos-img/chaqueta-niño.jpg",
+        "Nombre": "Chaqueta Para Niño",
+        "Descripcion": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor eligendi aliquam dignissimos quasi cum eveniet error quis minima sit quae. Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        "Precio": 100
+  }
+  
   return (
     <div className='App__container'>
-      <NavBar/>
-      <ItemListContainer greeting={'¡Bienvenidos!'}/>
+          <NavBar/>
+      <Router>
+        <Routes>
+            <Route path='/' element={<ItemListContainer greeting={'¡Bienvenidos!'}/>}/>
+            <Route path='/category/:id' element={<ItemListContainer greeting={'¡Bienvenidos!'}/>}/>
+            <Route path='/item/:id' element={<ItemDetailContainer/>}/>
+        </Routes>
+      
+      </Router>
     </div>
   );
 }
--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,4 +1,44 @@
+import { useParams } from "react-router-dom"; 
+import { useEffect, useState } from "react";
+
 const ItemDetailContainer = () => {
+
+    const {id} = useParams();
+    const [item, setItem] = useState({});
+
+
+    const urlProductos = window.location.origin + '/productos.json';
+
+    useEffect(() =>{
+        const obtenerData = async() =>{
+
+            const res = await fetch(urlProductos);
+            const data = await res.json();
+
+            data.map(item => {
+
+                if (item.id == id) {
+                    setItem(item)
+                };
+            });
+
+        }
+        obtenerData();
+
+    }, [])
+
+    return(
+        <div className="ItemDetailContainer">
+            <div className="ItemDetail__column1">
+                <img src={`${window.location.origin}${item.Imagen}`}/>
+            </div>
+            <div className="ItemDetail__column2">
+                <h3>{item.Nombre}</h3>
+                <p>{item.Descripcion}</p>
+                <h5>{item.Precio}</h5>
+            </div>
+        </div>
+    )
 
 }
 
